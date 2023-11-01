1624a4d8b9f1c8deedd24ea3f4510aa1b9c9354d
TuanNguyenDin <89063829+TuanNguyenDin@users.noreply.github.com>
2023-11-01 13:43:51 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
+  app.enableCors();
 
   // swagger setup
   const config = new DocumentBuilder()
