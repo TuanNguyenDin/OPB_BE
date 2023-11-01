bf1f590230a4c112b36e71291a2a50d89d430f36
TuanNguyenDin <89063829+TuanNguyenDin@users.noreply.github.com>
2023-11-01 12:21:43 +0700

    Update main.ts
    Setting CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   // swagger setup
   const config = new DocumentBuilder()
