f2438cafb2af0a9c177601d301e3e00c20e86292
TuanNguyenDin <89063829+TuanNguyenDin@users.noreply.github.com>
2023-11-01 14:00:48 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { resolve } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    "origin": "*",
+    "origin": "true",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "allowedHeaders": "Content-Type, Accept",
     "credentials": true,
