1d56b8f6a4a7ebf7e2826a99e21267712e5d86f2
Đinh Tuân Nguyễn <dinhtunann161@gmail.com>
2023-11-17 16:02:12 +0700

    Update food_order.controller.ts

--- src/food_order/food_order.controller.ts
+++ src/food_order/food_order.controller.ts
@@ -11,7 +11,7 @@ export class FoodOrderController {
 
   @Post('order/:id')
   @ApiOperation({ summary: 'Creates a food order' })
-  create(@Param('id') order_id: string, @Body() createFoodOrderDto: CreateFoodOrderDto, @Body() food_id: string) {
+  create(@Param('id') order_id: string, @Body() createFoodOrderDto: CreateFoodOrderDto, @Param() food_id: string) {
     return this.foodOrderService.create(createFoodOrderDto, order_id, food_id);
   }
 
