6912c96ebae1eaae5de4040c87f8b011e2a7005e
Đinh Tuân Nguyễn <dinhtunann161@gmail.com>
2023-12-19 16:24:22 +0700

    Update service_order.entity.ts

--- src/service_order/entities/service_order.entity.ts
+++ src/service_order/entities/service_order.entity.ts
@@ -1,9 +1,10 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import mongoose, { HydratedDocument } from "mongoose";
+import { Account } from "src/auth/entities/user.entities";
 import { Order } from "src/order/entities/order.entity";
 import { Service } from "src/service/entities/service.entity";
 
-@Schema()
+@Schema({timestamps: true})
 export class ServiceOrder {
     @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Service'})
     service_id: Service;
@@ -15,8 +16,8 @@ export class ServiceOrder {
     price: number;
     @Prop({})
     description: string;
-
-    timestamp: true;
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Account'})
+    created_by: Account;
 }
 export type serviceOrderDocuments = HydratedDocument<ServiceOrder>
 export const serviceOrderSchema = SchemaFactory.createForClass(ServiceOrder)
