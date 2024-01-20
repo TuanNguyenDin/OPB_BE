147ae3a246be706a8dfc50a91a2de3c252b6dff9
Đinh Tuân Nguyễn <dinhtunann161@gmail.com>
2024-01-20 12:02:55 +0700

    Update order.service.ts

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -35,12 +35,38 @@ export class OrderService {
         isRead: false
       });
       console.log(notify);
-      
+
     })
     return await this.orderModel.create(createOrderDto);
   }
 
   async findAll() {
+    try {
+      const expiredOrders = await this.orderModel.find({ ended_at: { $lt: new Date() }, status: { $nin: ['done', 'canceled'] } }).exec();
+      expiredOrders.forEach(async (order) => {
+        const notify = await this.NotifyModel.create({
+          title: 'Đơn hàng đã quá hạn',
+          content: `Đơn hàng ${order._id} đã quá hạn`,
+          send_to: order.customer_id,
+          created_by: 'system',
+          isRead: false
+        });
+
+        const managers = await this.accountModel.find({ role: 'manager', status: 'activated' }).exec();
+        managers.forEach(async (manager) => {
+          const managerNotify = await this.NotifyModel.create({
+            title: 'Đơn hàng đã quá hạn',
+            content: `Đơn hàng ${order._id} đã quá hạn`,
+            send_to: manager._id,
+            created_by: 'system',
+            isRead: false
+          });
+        });
+      });
+    } catch (error) {
+      console.log(error);
+
+    }
     return await this.orderModel.find().populate('customer_id').populate('restaurant_id').exec();
   }
 
